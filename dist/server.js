de8f1d983e59e15905da03e206f71c9b8d1d2590
kyanrong <kekyanrong@gmail.com>
2020-03-21 14:40:25 +0800

    Modified .gitignore

---
+++ dist/server.js
@@ -0,0 +1,32 @@
+"use strict";
+const express = require('express');
+const fs = require('fs');
+const sortBy = require('lodash.sortby');
+const path = require('path');
+const app = express();
+const port = 3000;
+app.use(express.static('dist'));
+app.get('/stations', (req, res) => {
+    const file = fs.readFileSync(path.join(__dirname, '../src/assets/train-station-chinese-names.csv'), 'utf-8');
+    let data = file.split('\n').splice(1);
+    data = data.map((x) => {
+        const line = x.split(',');
+        return ({
+            value: line[0],
+            label: line[1],
+            label_chinese: line[2],
+        });
+    });
+    data = sortBy(data, ['label']);
+    res.send(data);
+});
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.listen(port, (err) => {
+    if (err) {
+        return console.error(err);
+    }
+    return console.log(`server is listening on ${port}`);
+});
+//# sourceMappingURL=server.js.map
