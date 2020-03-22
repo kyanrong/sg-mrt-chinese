30d0b56e6e038508cd28feabb44a82921ef5a5bc
kyanrong <kekyanrong@gmail.com>
2020-03-22 20:56:15 +0800

    Updated store

--- src/app/Store.ts
+++ src/app/Store.ts
@@ -3,20 +3,12 @@ import thunk from 'redux-thunk';
 
 import reducer from './reducer';
 
-let composed = compose(
-  applyMiddleware(thunk)
-);
-
-if (process.env.NODE_ENV !== 'production') {
-  composed = compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
-}
-
 const store = createStore(
   reducer,
-  composed
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+  )
 );
 
 export default store;
