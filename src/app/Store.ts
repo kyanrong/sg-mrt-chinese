ff8c7ef5176fc817f5bca7773a60ad0a1b573c50
kyanrong <kekyanrong@gmail.com>
2020-03-22 20:46:38 +0800

    Account for process.env.NODE_ENV

--- src/app/Store.ts
+++ src/app/Store.ts
@@ -3,11 +3,20 @@ import thunk from 'redux-thunk';
 
 import reducer from './reducer';
 
-const store = createStore(
-  reducer,
-  compose(
+let composed = compose(
+  applyMiddleware(thunk)
+);
+
+if (process.env.NODE_ENV !== 'production') {
+  composed = compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  ));
+  );
+}
+
+const store = createStore(
+  reducer,
+  composed
+);
 
 export default store;
